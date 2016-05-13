962f1845198516c06654c8a89c1e2703283afaf0
Blank010111 <blank32702@gmail.com>
2016-05-13 12:46:23 -0400

    Update minimax.js

--- javascripts/minimax.js
+++ javascripts/minimax.js
@@ -1,1 +1,12 @@
+for (i=0,c = [],d = []; i<grid,i++)
+for (j=0,a = [],b = []; j<grid,j++)
+{
+  a.push(i*grid+j); 
+  b.push(j*grid+i); 
+}
+combo.push(a,b)
+c.push(i*grid*i);
+d.push((grid-i-1)*grid+1);
 
+ 
+ 
