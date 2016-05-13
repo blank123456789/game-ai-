86be959a48f64b83fdb59658f552bff4dae10095
leviu02 <leviu02@gmail.com>
2016-05-13 12:40:34 -0400

    Update tictactoe-ai.js

--- javascripts/tictactoe-ai.js
+++ javascripts/tictactoe-ai.js
@@ -4,6 +4,4 @@ ctx.fillStyle = "#2F13B6";
 ctx.fillRect(0, 0, 80, 80);
 
 var board = document.getElementByClass("board");
-var xPos = pos.clientX;
-var yPos = pos.clientY;
-ctx.drawImage(board, xPos, yPos);
+ctx.drawImage(board, 50, 50);
