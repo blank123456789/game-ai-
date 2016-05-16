1519b1169c5c16e7c879170def0640b8be554741
Blank010111 <blank32702@gmail.com>
2016-05-16 19:18:09 -0400

    Update tictactoe-ai.js

--- javascripts/tictactoe-ai.js
+++ javascripts/tictactoe-ai.js
@@ -1,10 +1,10 @@
 var canvas = document.getElementById("tictaccanvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#2F13B6";
-ctx.fillRect(0, 0, 650, 600);
+ctx.fillRect(0, 0, 600, 600);
 var imageObj = new Image();
 imageObj.src="/game-ai-/images/tictactoe_board.png";
   imageObj.onload = function() {
-    ctx.drawImage(imageObj,100,0,200,200);
+    ctx.drawImage(imageObj,90,0,200,200);
   };
   
