318b631c80853a684bd1fe31f6dee5007782bf83
Blank010111 <blank32702@gmail.com>
2016-05-15 22:13:20 -0400

    Update tictactoe-ai.js

--- javascripts/tictactoe-ai.js
+++ javascripts/tictactoe-ai.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("tictaccanvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#2F13B6";
-ctx.fillRect(0, 0, 800, 800);
+ctx.fillRect(0, 0, 600, 800);
 
 var board = document.getElementByClass("board");
 ctx.drawImage(board, 60, 60);
