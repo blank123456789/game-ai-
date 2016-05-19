819975fbc03e465dbedd95e7d514b3175ce5e977
Blank010111 <blank32702@gmail.com>
2016-05-18 21:19:52 -0400

    Update tictactoe-ai.js

--- javascripts/tictactoe-ai.js
+++ javascripts/tictactoe-ai.js
@@ -7,26 +7,4 @@ imageObj.src="/game-ai-/images/tictactoe_board.png";
   imageObj.onload = function() {
     ctx=drawImage(imageObj,0,0,300,150)
   };
-  function Tile(x,y) {
-            var x= mouseX;var y= mouseY 
-            var tile=Tile.Blank
-            if(tile == null) {
-              var _c= document.createElement("canvas");
-              _c.width=_c.height=100;
-              _ctx=_c.getContext("2d");
-              _ctx.fillstyle="skyblue";
-              _ctx.fillRect(0,0,100,100);
-              Tile.Blank = new Image();
-              Tile.Blank.src =_c.toDataUrl();
-            }
-            this.update= function(){
-            };
-            this.draw=function(ctx){
-              _ctx.drawImage(tile,x,y);
-            }
-           }
-           var data;
-           function init(){
-             data= new Tile(0,0);
-           }
-    
+  
