b95b08fa247e9cc2baa17f9c8c290a12e87ba254
leviu02 <leviu02@gmail.com>
2016-05-18 22:15:20 -0400

    Update tictactoe-ai.js

--- javascripts/tictactoe-ai.js
+++ javascripts/tictactoe-ai.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FFFFFF";
 ctx.fillRect(0, 0, 500, 500);
 var imageObj = new Image();
-imageObj.src="/game-ai-/images/tictactoe_O.png";
+imageObj.src="/game-ai-/images/tictactoe_o.png";
   imageObj.onload = function() {
     ctx.drawImage(imageObj,0,0,100,100)
   };
