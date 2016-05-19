ba240e32b10cc7baae72ca8a05e65f9f3cd5e2b7
Blank010111 <blank32702@gmail.com>
2016-05-18 21:25:06 -0400

    Update tictactoe-ai.js

--- javascripts/tictactoe-ai.js
+++ javascripts/tictactoe-ai.js
@@ -5,5 +5,5 @@ ctx.fillRect(0, 0, 500, 500);
 var imageObj = new Image();
 imageObj.src="/game-ai-/images/tictactoe_O.png";
   imageObj.onload = function() {
-    ctx.drawImage(imageObj1,0,0,100,100)
+    ctx.drawImage(imageObj,0,0,100,100)
   };
