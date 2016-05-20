a87ca725189fbde059ff672ffb41dad95c310e55
leviu02 <leviu02@gmail.com>
2016-05-20 12:40:43 -0400

    Update tictactoe-ai.js

--- javascripts/tictactoe-ai.js
+++ javascripts/tictactoe-ai.js
@@ -4,7 +4,7 @@ ctx.fillStyle = "#FFFFFF";
 ctx.fillRect(0, 0, 600, 600);
 
 ctx.beginPath();
-ctx.lineWidth="1";
-ctx.strokeStyle="#32EDF2";
-ctx.rect(5,5,100,100); 
+ctx.fillStyle="#32EDF2";
+ctx.fillRect(5,5,100,100); 
 ctx.stroke();
+ctx.closePath
