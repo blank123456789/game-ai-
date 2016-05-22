d9162b26ec744dd12b90bb079d5021ac194d0efa
leviu02 <leviu02@gmail.com>
2016-05-21 22:34:33 -0400

    Update tictactoe-ai.js

--- javascripts/tictactoe-ai.js
+++ javascripts/tictactoe-ai.js
@@ -2,7 +2,7 @@ var canvas = document.createElement('canvas');
 canvas.id = "tictaccanvas";
 canvas.width = 600;
 canvas.height = 600;
-canvas.setAttribute('style', "position: absolute;  left: 50%;top: 50%;right:50%;bottom:50%;border:2px solid blue;z-index:1");
+canvas.setAttribute('style', "position: absolute;  left: 0%;top: 0%;right:0%;bottom:0%;border:2px solid blue;z-index:1");
 var body = document.getElementsByTagName("body")[0];
 body.appendChild(canvas);
 
