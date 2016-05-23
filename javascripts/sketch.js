706bf9d6376e86c979731a6df5215312b2e90499
leviu02 <leviu02@gmail.com>
2016-05-23 17:39:31 -0400

    Update sketch.js

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -37,8 +37,20 @@ function createBoard(x,y){
 }
 
 function takeTurn() {
-	turns+=1;
-	player1_turn = false;
+	if (mouseX > 0 && mouseX < width/3) {
+		if (mouseY > 0 && mouseY < height/3) 
+		{
+			drawShape(width/6,height/6,player1_turn,1)
+		}
+		else if (mouseY > height/3 && mouseY < 2*height/3) 
+		{
+			drawShape(width/6,3*height/6,player1_turn,2)
+		}
+		else if(mouseY > 2*height/3 && mouseY < height) 
+		{
+			drawShape(width/6,5*height/6,player1_turn,3)
+		}
+	}
 }
 
 function draw() {
