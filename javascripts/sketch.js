5e67a9356af264699951ce55432fe9287e869060
leviu02 <leviu02@gmail.com>
2016-05-23 16:52:32 -0400

    Update sketch.js

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -1,4 +1,6 @@
 function setup() {
+	var centerx = (windowWidth - width) / 2;
+	var centery = (windowHeight - height) / 2;
   var board = createBoard(380,380);
   boardx = (windowWidth - width) / 2;
   boardy = ((windowHeight - height) / 2) + 100;
