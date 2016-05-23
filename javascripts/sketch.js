ecb079223631f7d7adba8cb43d7c635d4dec0d64
leviu02 <leviu02@gmail.com>
2016-05-23 16:30:11 -0400

    Update sketch.js

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -1,10 +1,10 @@
 function setup() {
-  var c = createCanvas(380, 380);
-  cx = (windowWidth - width) / 2;
-  cy = ((windowHeight - height) / 2) + 100;
-  c.position(cx, cy);
+  var board = createBoard(380,380);
+  boardx = (windowWidth - width) / 2;
+  boardy = ((windowHeight - height) / 2) + 100;
+  board.position(boardx, boardy);
   background(51);
-  createBoard(380,380);
+  
 }
 function createBoard(x,y){
 	var board = createCanvas(x,y);
