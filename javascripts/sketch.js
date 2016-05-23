52d5b9e498baa1d9a2a77014bae1bc6a68fc82cd
leviu02 <leviu02@gmail.com>
2016-05-23 16:27:53 -0400

    Update sketch.js

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -4,6 +4,20 @@ function setup() {
   cy = ((windowHeight - height) / 2) + 100;
   c.position(cx, cy);
   background(51);
+  createBoard(380,380);
+}
+function createBoard(x,y){
+	var board = createCanvas(x,y);
+	background(255)
+	for (i = 0; i < 4; i++)
+	{
+		line((i*x)/3,0,(i*x)/3,y);
+	}
+	for (i = 0; i < 4; i++)
+	{
+		line(0,(i*y)/3,x,(i*y)/3);
+	}
+	return board;
 }
 
 function draw() {
