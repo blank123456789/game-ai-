fa8c8cd0b206b0b1e1d974d77562adf376a60752
leviu02 <leviu02@gmail.com>
2016-05-23 16:22:58 -0400

    Update sketch.js

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -1,7 +1,7 @@
 function setup() {
   var c = createCanvas(380, 380);
   cx = (windowWidth - width) / 2;
-  cy = ((windowHeight - height) / 2) + 200;
+  cy = ((windowHeight - height) / 2) + 100;
   c.position(cx, cy);
   background(51);
 }
