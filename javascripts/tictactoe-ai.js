9d4da9bb6a91d1adfdfc35a0c46e7cfa5240c51b
leviu02 <leviu02@gmail.com>
2016-05-23 12:30:07 -0400

    Update tictactoe-ai.js

--- javascripts/tictactoe-ai.js
+++ javascripts/tictactoe-ai.js
@@ -2,6 +2,7 @@ var canvas = document.createElement('canvas');
 canvas.id = "tictaccanvas";
 canvas.width = 380;
 canvas.height = 380;
+ctx.clearRect(0,0,380,380);
 canvas.setAttribute('style', "position:absolute;margin:auto;left:0%;top:25%;right:0%;bottom:0%;border:2px solid blue;z-index:2;background-color:white");
 var body = document.getElementsByTagName("body")[0];
 body.appendChild(canvas);
@@ -18,7 +19,6 @@ var drawTile = function(x,y) {
 }
 
 var draw = function() {
-  ctx.clearRect(0,0,380,380);
 }
 
 setInterval(draw,10)
