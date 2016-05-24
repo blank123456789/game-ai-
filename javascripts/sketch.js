b95431f1aa0d8646e6e5bb74597bc944e6cc3cce
leviu02 <leviu02@gmail.com>
2016-05-23 21:36:00 -0400

    Update sketch.js

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -106,7 +106,7 @@ function drawShape(cx,cy,turn1,idx) {
 			rectMode(CENTER);
 			fill(255);
 			//rect(cx,cy,50,50);
-			image(img, cx, cy,image.height/4,image.width/4);
+			image(img, cx, cy,img.width/4,img.height/4);
 			board_array[idx] = 'x';
 		}
 	}
