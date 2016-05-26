9c240ad1a208331d3e16035cf07d3ea24c1b9509
leviu02 <leviu02@gmail.com>
2016-05-26 12:34:59 -0400

    Update impossible.js

--- javascripts/impossible.js
+++ javascripts/impossible.js
@@ -1,5 +1,10 @@
-function draw() {
+function setup() {
+  canvas = createCanvas(400,400);
+  canvas.position(canvasX,canvasY);
+  canvasX = (windowWidth - width) / 2;
+  canvasY = ((windowHeight - height) / 2) + 100;
+  background(255);
 }
 
-function setup() {
+function draw() {
 }
